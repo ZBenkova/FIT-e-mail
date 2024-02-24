230da01ad0f72e0f96cd2c0a8b681f400bbece87
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-02-24 22:47:44 +0100

    hotovo

---
+++ script.js
@@ -0,0 +1,13 @@
+let jmeno = prompt("Zadej krestni jmeno bez diakritiky.")
+let prijmeni = prompt("Zadej prijmeni bez diakritiky.")
+
+jmeno = jmeno.trim()
+prijmeni = prijmeni.trim()
+jmeno = jmeno.slice(0, 3)
+prijmeni = prijmeni.slice(0, 5)
+
+let email = prijmeni + jmeno 
+email = email.toLowerCase()
+
+
+document.body.innerHTML = email + "@fit.cvut.cz"
